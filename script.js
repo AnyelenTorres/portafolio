5d4a109d441d30857607d8d9f64becb67145358a
AnyelenTorres <giselanyelentorres@gmail.com>
2023-02-07 16:57:24 -0300

    subiendo desde computadora nueva

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function mostrarInteriorAboutMe(){
+    document.getElementById("interiorAboutMe").style.display="block";
+
+}
+function mostrarInteriorConocimientos(){
+    document.getElementById("interiorConocimientos").style.display="block";
+
+}
+function mostrarInteriorHardSkills(){
+    document.getElementById("interiorHardSkills").style.display="block";
+}
+ function mostrarInteriorIdiomas(){
+    document.getElementById("interiorIdiomas").style.display="block";
+}
+function mostrarInteriorSoftSkills(){
+    document.getElementById("interiorSoftSkills").style.display="block";
+
+}
+function mostrarInteriorPasatiempo(){
+    document.getElementById("interiorPasatiempo").style.display="block";
+
+}
+function mostrarInteriorProyectos(){
+    document.getElementById("interiorProyectos").style.display="block";
+}
