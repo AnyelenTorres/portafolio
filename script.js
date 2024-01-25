ff73e0e39f8f120f3eab2abf228539baeaef3dcf
anyelentorres <giselanyelentorres@gmail.com>
2024-01-25 19:46:42 -0300

    agregando proyectos

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
-function mostrarInteriorAboutMe(){
-    document.getElementById("interiorAboutMe").style.display="block";
+const interior = document.getElementsByClassName("interior");
 
-}
+
+function mostrarInteriorAboutMe(){
+   document.getElementById("interiorAboutMe").style.display="block";
+   
+} 
 function mostrarInteriorConocimientos(){
     document.getElementById("interiorConocimientos").style.display="block";
-    if (event.target = onclick){
-
-        document.getElementById("interiorConocimientos").style.display="none";
-    }
+    
 }
 function mostrarInteriorHardSkills(){
     document.getElementById("interiorHardSkills").style.display="block";
@@ -20,11 +20,25 @@ function mostrarInteriorSoftSkills(){
     document.getElementById("interiorSoftSkills").style.display="block";
 
 }
-function mostrarInteriorPasatiempo(){
-    document.getElementById("interiorPasatiempo").style.display="block";
 
-}
 function mostrarInteriorProyectos(){
     document.getElementById("interiorProyectos").style.display="block";
 }
 
+
+function cerrarAboutMe(){
+    document.getElementById("interiorAboutMe").style.display="none";
+}
+function cerrarConocimientos(){
+    document.getElementById("interiorConocimientos").style.display="none";
+}
+    function cerrarHardSkills(){
+    document.getElementById("interiorHardSkills").style.display="none";
+}
+    function cerrarIdiomas(){
+    document.getElementById("interiorIdiomas").style.display="none";
+}
+    function cerrarSoftSkills(){
+    document.getElementById("interiorSoftSkills").style.display="none";
+    
+}
